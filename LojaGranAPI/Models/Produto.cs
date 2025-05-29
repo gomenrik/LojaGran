6d790d7a2bd80b20c9cfad3aba2bf63a9d041f34
gomenrik <campanhahenrique13@gmail.com>
2025-05-29 19:57:50 -0300

    added requireds

--- LojaGranAPI/Models/Produto.cs
+++ LojaGranAPI/Models/Produto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,36 @@ namespace LojaGranAPI.Models
 {
     public class Produto
     {
-        
+        [Key]
+
+        public int Id { get; set; }
+
+        [Required]
+
+        public int CategoriaId { get; set; }
+
+        [Required]
+
+        public string Nome { get; set; }
+
+        [Required]
+
+        public string Descricao { get; set; }
+
+        [Required]
+
+        public int Estoque { get; set; }
+
+        [Required]
+
+        public decimal ValorCusto { get; set; }
+
+        [Required]
+
+        public decimal ValorVenda { get; set; }
+
+        [Required]
+
+        public bool Destaque { get; set; }
     }
 }
