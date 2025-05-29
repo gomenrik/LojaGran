ea61b72ba6dd272a03b98cbae141fb02fca02381
gomenrik <campanhahenrique13@gmail.com>
2025-05-29 20:31:21 -0300

    added produtoscontroller

---
+++ LojaGranAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaGranAPI.Data;
+using LojaGranAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaGranAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProdutosController(AppDbContext db) : ControllerBase
+
+{
+    public class P
+    {
+
+    }
+}
