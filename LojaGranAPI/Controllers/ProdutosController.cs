170ca515dfed37b5f290e3a296d01f34c66d8967
gomenrik <campanhahenrique13@gmail.com>
2025-05-30 16:09:08 -0300

    produtoscontroller done

--- LojaGranAPI/Controllers/ProdutosController.cs
+++ LojaGranAPI/Controllers/ProdutosController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using LojaGranAPI.Data;
 using LojaGranAPI.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +7,52 @@ namespace LojaGranAPI.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class ProdutosController(AppDbContext db) : ControllerBase
-
 {
-    public class P
-    {
+    private readonly AppDbContext _db = db;
 
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(_db.Produtos.ToList());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var produto = _db.Produtos.Find(id);
+        if (produto == null)
+            return NotFound();
+        return Ok(produto);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] Produto produto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Produto informado com problemas");
+        _db.Produtos.Add(produto);
+        _db.SaveChanges();
+        return CreatedAtAction(nameof(Get), new { id = produto.Id }, produto);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Edit(int id, [FromBody] Produto produto)
+    {
+        if (!ModelState.IsValid || id != produto.Id)
+            return BadRequest("Produto informado com problemas");
+        _db.Produtos.Update(produto);
+        _db.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var produto = _db.Produtos.Find(id);
+        if (produto == null)
+            return NotFound();
+        _db.Produtos.Remove(produto);
+        _db.SaveChanges();
+        return NoContent();
     }
 }
